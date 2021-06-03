ae06cd17221531086366e92d7ad47523bb760112
Tom Eloe <tomeloe@gmail.com>
2021-06-03 16:20:59 -0700

    restructure files/add gulp-minify task/add production css/js to ./dist dir

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,44 @@ const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const browserSync = require('browser-sync').create();
+const minify = require('gulp-minify');
 
 // Compile scss into css/ add vendor prefixes/ compress css
 function style() {
     return gulp
-        .src('./scss/**/*.scss')
+        .src('app/scss/**/*.scss')
         .pipe(sourcemaps.init())
         .pipe(sass())
         .on('error', sass.logError)
         .pipe(postcss([autoprefixer(), cssnano()]))
         .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
+}
+
+// Minify js
+function minifyjs() {
+    return gulp
+        .src('app/js/snazzymenu.js', { allowEmpty: true })
+        .pipe(minify({ noSource: true }))
+        .pipe(gulp.dest('dist/js'));
 }
 
 // Watch files
 function watch() {
     browserSync.init({
         server: {
-            baseDir: './',
-            index: './index.html',
+            baseDir: './app',
+            index: '/index.html',
         },
     });
-    gulp.watch('./scss/**/*.scss', style);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('app/scss/**/*.scss', style);
+    gulp.watch('app/js/**/*.js', minifyjs);
+    gulp.watch('app/*.html').on('change', browserSync.reload);
+    gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
 }
 
 // Export tasks
 exports.style = style;
+exports.minifyjs = minifyjs;
 exports.watch = watch;
-
