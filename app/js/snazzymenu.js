e4d90b8a2ee8614916cf7fbfc0ded5bea081ff6b
Tom Eloe <tomeloe@gmail.com>
2022-02-04 22:39:32 -0800

    add accessibility attrs to buttons

--- app/js/snazzymenu.js
+++ app/js/snazzymenu.js
@@ -75,9 +75,9 @@
 
             // Load .cta/ .controls/ .close-overlay dynamically
             nav.find('.menu').before(
-                '<div class="controls"><div class="logo"><a href="/" title="">' +
+                '<div class="controls"><div class="logo"><a href="/" title="" aria-label="Home">' +
                     homeBtn +
-                    '</a></div><div class="cta"><button class="menu-toggle"></button></div></div>'
+                    '</a></div><div class="cta"><button class="menu-toggle" aria-label="Menu"></button></div></div>'
             );
             nav.after('<div class="close-overlay"></div>');
 
@@ -92,7 +92,7 @@
                     '<a href="' +
                     settings.locationBtn +
                     '" class="location-btn-mobile ' +
-                    '" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg> <span>' +
+                    '" target="_blank" aria-label="Location"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg> <span>' +
                     locationLabel +
                     '</span></a>';
                 nav.find('.cta').prepend(btn);
@@ -104,7 +104,7 @@
                     '<a href="tel:' +
                     settings.phoneBtn +
                     '" class="call-btn-mobile ' +
-                    '"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg> <span>' +
+                    '" aria-label="Call"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 22.621l-3.521-6.795c-.008.004-1.974.97-2.064 1.011-2.24 1.086-6.799-7.82-4.609-8.994l2.083-1.026-3.493-6.817-2.106 1.039c-7.202 3.755 4.233 25.982 11.6 22.615.121-.055 2.102-1.029 2.11-1.033z"/></svg> <span>' +
                     phoneLabel +
                     '</span></a>';
                 nav.find('.cta').prepend(btn);
